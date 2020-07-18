44e8223b4bedb82ded522b4d402af01ffb2edda3
Jack Li <jack@gatik.ai>
2020-07-17 22:55:07 -0400

    save

--- install_elementary.sh
+++ install_elementary.sh
@@ -5,8 +5,6 @@ sudo add-apt-repository ppa:elementary-os/os-patches
 sudo apt update
 sudo apt install elementary-desktop
 
-# sudo apt-get install pantheon-greeter
-
 # Enable Pantheon greeter, change the order # to be the largest, i.e. 51 instead of 40
 echo /usr/share/lightdm/lightdm.conf.d/40-io.elementary.greeter.conf
 
@@ -18,5 +16,9 @@ echo GTK_THEME=elementary:dark subl
 
 # Set backlight so it is not always full, some NVIDIA driver bug
 sudo apt-get install -y xbacklight
+
+# Acceleration Profile for Trackpoint
+# xinput set-prop "$device" 'libinput Accel Profile Enabled' {0,1}
+# xinput set-prop "$device" 'libinput Accel Speed' 0.5
 
 # Use GPU for Xorg, prevent gala using too much CPU
