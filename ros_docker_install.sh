475bda05c3682cd96bbbd71ba5c0c65a95842f58
Jack Li <jack@gatik.ai>
2020-11-11 11:27:33 -0500

    update

--- ros_docker_install.sh
+++ ros_docker_install.sh
@@ -17,6 +17,14 @@ sudo apt-get install -y htop
 sudo apt-get install -y ros-${ROS_DISTRO}-rqt*
 sudo apt-get install -y byobu
 
+sudo apt-get install -y libasound-dev
+
+sudo apt-get install -y clang-format
+sudo apt-get install -y gdb
+
+sudo apt install -y pcl-tools
+sudo apt install -y nautilus
+
 # wget -O vscode.deb https://az764295.vo.msecnd.net/stable/17299e413d5590b14ab0340ea477cdd86ff13daf/code_1.47.2-1594837870_amd64.deb
 sudo apt-get install -y ./vscode.deb
 code --install-extension Shan.code-settings-sync
