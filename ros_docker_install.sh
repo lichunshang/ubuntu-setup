45c560ce84e76c090e60724bfc22688f409463cd
Jack Li <jack@gatik.ai>
2020-11-11 11:56:30 -0500

    update

--- ros_docker_install.sh
+++ ros_docker_install.sh
@@ -18,7 +18,7 @@ then
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 	sudo apt update
-	sudo apt install code
+	sudo apt install -y code
 fi
 
 sudo apt-get install -y wget
