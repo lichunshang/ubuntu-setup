5b22d019160f446c9d7088d64aa962cc65137697
Jack Li <jack@gatik.ai>
2020-11-11 11:37:51 -0500

    update

--- ros_docker_install.sh
+++ ros_docker_install.sh
@@ -8,8 +8,10 @@ set -eu
 git config --global user.email "jack@gatik.ai"
 git config --global user.name "Jack Li"
 
-sudo cp -R -u -p ./icons /usr/share
-sudo cp -R -u -p ./fonts /usr/share
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd v)"
+
+sudo cp -R -u -p ${SCRIPT_DIR}/icons /usr/share
+sudo cp -R -u -p ${SCRIPT_DIR}/fonts /usr/share
 
 sudo apt-get update
 sudo apt-get install -y wget
@@ -25,6 +27,13 @@ sudo apt-get install -y gdb
 sudo apt install -y pcl-tools
 sudo apt install -y nautilus
 
-# wget -O vscode.deb https://az764295.vo.msecnd.net/stable/17299e413d5590b14ab0340ea477cdd86ff13daf/code_1.47.2-1594837870_amd64.deb
-sudo apt-get install -y ./vscode.deb
-code --install-extension Shan.code-settings-sync
+sudo bash -c 'echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf'
+sudo sysctl -p
+cat /proc/sys/fs/inotify/max_user_watches
+
+if ! command -v vscode &> /dev/null
+then
+	wget -O vscode.deb https://az764295.vo.msecnd.net/stable/17299e413d5590b14ab0340ea477cdd86ff13daf/code_1.47.2-1594837870_amd64.deb
+	sudo apt-get install -y ./vscode.deb
+	code --install-extension Shan.code-settings-sync
+fi
